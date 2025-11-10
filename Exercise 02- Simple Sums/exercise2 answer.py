016703d463b652a24f4ec9f5a06b6963ffb68d76
ytboytubeee-jpgalin <ytboytubeee@gmail.com>
2025-11-10 13:02:28 +0400

    Create exercise2 answer.py

---
+++ Exercise 02- Simple Sums/exercise2 answer.py
@@ -0,0 +1,11 @@
+# Step 1: Declare a variable and initialize it with the integer value 8
+num1 = 8
+
+# Step 2: Declare a second variable and initialize it with the integer value 10
+num2 = 10
+
+# Step 3: Declare a third variable that stores the sum of the first two numbers
+sum_result = num1 + num2
+
+# Step 4: Print the value of the sum to the console
+print(sum_result)
