7afcdcd4eb67d1be1bdd1e9425e5d6ea6c2f4dc0
ytboytubeee-jpgalin <ytboytubeee@gmail.com>
2025-11-17 11:15:20 +0400

    Create exercise7 answer.py

---
+++ Exercise 07 - Some Counting/exercise7 answer.py
@@ -0,0 +1,26 @@
+# Exercise 7: Some Counting
+
+print("1. Counting up from 0 to 50 (increments of 1):")
+for i in range(0, 51, 1):
+    print(i, end=" ")
+print("\n")  # New line for separation
+
+print("2. Counting down from 50 to 0 (decrements of 1):")
+for i in range(50, -1, -1):
+    print(i, end=" ")
+print("\n")
+
+print("3. Counting up from 30 to 50 (increments of 1):")
+for i in range(30, 51, 1):
+    print(i, end=" ")
+print("\n")
+
+print("4. Counting down from 50 to 10 (decrements of 2):")
+for i in range(50, 9, -2):
+    print(i, end=" ")
+print("\n")
+
+print("5. Counting up from 100 to 200 (increments of 5):")
+for i in range(100, 201, 5):
+    print(i, end=" ")
+print("\n")
