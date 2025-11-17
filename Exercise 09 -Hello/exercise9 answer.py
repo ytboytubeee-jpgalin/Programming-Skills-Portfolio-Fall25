532f740cce276c93e44e1b837dde2368d22adffb
ytboytubeee-jpgalin <ytboytubeee@gmail.com>
2025-11-17 11:15:28 +0400

    Create exercise9 answer.py

---
+++ Exercise 09 -Hello/exercise9 answer.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello")  # Fill in this blank to print "Hello" to the console
+
+def main():
+    hello()  # Call the hello() function
+
+if __name__ == "__main__":
+    main()
